2b461846c075b8b25097fce6af2229f3a308e7db
satyam lodhi <satyamlodhi650@gmail.com>
2024-10-10 14:21:17 +0000

    conversion of datatype

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33"
+//note:- //let score = 33 :- this is written in loyer case
+//note:- //let score ="33" :- this is written in 'string' because the number 33 is undercoded("").
+
+/*console.log(typeof score);
+console.log(typeof (score));
+*/
+/* let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+*/
+//note :- // conversion :-
+//console.log(valueInNumber); // out :- NaN
+// "33" => 33
+// "33abc" => NaN
+// ture => 1; false =>0
+let isLoggedIn = "satyam"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+ 
+//note :- 
+// 1 => ture; 0 => false
+// "" => false 
+// "satyam" => true 
+
+let someNumber = 33
+let stringNumber =String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
