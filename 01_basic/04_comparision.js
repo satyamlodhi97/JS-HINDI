63ac4bcf67422869e0eded0478991ab77e635b51
satyam lodhi <satyamlodhi650@gmail.com>
2024-10-11 06:20:37 +0000

    comparision of datatyepes

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1); //output:-true
+// console.log(2 >= 1);//output:-true
+// console.log(2 < 1);//output:-false
+// console.log(2 == 1);//output:-false
+// console.log(2 != 1);//output:-true
+ 
+//console.log("2">1); //output:-true
+//console.log("02">1); //output:-true
+
+// we are avoie the these comparision
+console.log(null > 0);//output:-false
+console.log(null == 0);//output:-false
+console.log(null >= 0);//output:-true
+
+console.log(undefined > 0); //output:-false
+console.log(undefined == 0);//output:-false
+console.log(undefined >= 0);//output:-false
+
+//strick check // ====
+console.log("2"===1);
