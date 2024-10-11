d753d8cf44d719a3cc7913249f32145da89f6626
satyam lodhi <satyamlodhi650@gmail.com>
2024-10-11 11:28:58 +0000

    Strings

--- 01_basic/05_strings.js
+++ 01_basic/05_strings.js
@@ -9,11 +9,34 @@ const repoCount = 12
 //this centace is interpulated
 console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String ('satyam')
+const gameName = new String ('satyam-ram-the')
 
-console.log(gameName[0]); //output:- s
-console.log(gameName.__proto__); //output:-{} this isn't empty in {} it have more value
+// console.log(gameName[0]); //output:- s
+// console.log(gameName.__proto__); //output:-{} this isn't empty in {} it have more value
 
-//method// 
-console.log(gameName.length);//output:-6
-console.log(gameName.toUpperCase());//output:-SATYAM
+// //method// 
+// console.log(gameName.length);//output:-6
+// console.log(gameName.toUpperCase());//output:-SATYAM
+// console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+//jitne word lena ho utne le sakte hain
+const newString = gameName.substring(0, 4);
+console.log(newString);
+
+const anotherString = gameName.slice(-4, 4);
+console.log(anotherString);
+
+const newString1 = "   satyam    "
+console.log(newString1);//output:-     satyam    
+console.log(newString1.trim());//output:-satyam//trim se space hat jate hai
+
+const url = "https://satyam.com/satyam%20lodhi"
+
+console.log(url.replace('%20','-'));
+//output:-https://satyam.com/satyam-lodhi
+
+console.log(url.includes('satyam'));//output:-true
+
+//change in array gameName
+console.log(gameName.split('m'));//word ko remove karke array me change kar deta hai
